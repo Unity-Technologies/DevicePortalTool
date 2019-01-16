66917737ba9c0ce8b4054f171a283fa487b99171
Tim Keosababian <timkeo@users.noreply.github.com>
2019-01-16 12:32:56 -0800

    Use Linux commands to perform zip operation
    The "Completed" builder on Katana uses Linux machines instead of Windows; updating script to match.

--- create_zip.pl
+++ create_zip.pl
@@ -8,5 +8,5 @@ mkdir "binaries";
 
 chdir "builds";
 
-system("..\\tools\\7z.exe", "a", "-r" , "builds.zip", "*.exe", "*.pdb", "version.txt") eq 0 or die("failed creating builds.zip");
-system("move", "builds.zip", "..\\binaries");
+system("zip -rq builds.zip . -i *.exe *.pdb version.txt") && die("failed creating builds.zip");
+system("mv builds.zip ../binaries");
