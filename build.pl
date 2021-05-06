5a3273b9df228a814e418d58ece53b5d7ee90e87
Richard Fine <richardf@unity3d.com>
2021-05-05 23:46:12 -0400

    Add newline

--- build.pl
+++ build.pl
@@ -11,7 +11,7 @@ mkpath($outDirectory);
 # write build version.txt to output dir
 open(my $versionFile, '>', "$outDirectory\\version.txt") or die("Unable to write build information to version.txt");
 print $versionFile "Repository: https://github.com/Unity-Technologies/DevicePortalTool\n";
-print $versionFile "Branch: " . $ENV{GIT_BRANCH};
+print $versionFile "Branch: " . $ENV{GIT_BRANCH} . "\n";
 print $versionFile "Revision: " . $ENV{GIT_REVISION};
 close $versionFile;
 
